72e30390f7ea6a3c9c8638a44b120bae9442ab26
Lukas S <SchalkLukas20@gmail.com>
2017-03-13 22:06:32 +0100

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /**
  * 12.03.2017
@@ -13,6 +15,7 @@ public class GUI {
     private JTextField inputPoly1, inputPoly2;
     private JLabel output_lbl;
     private JTextArea outputArea;
+    private JLabel version_lbl;
 
     public GUI()  {
         jf = new JFrame();
@@ -40,24 +43,32 @@ public class GUI {
 
         startBtn = new JButton("Berechne");
         startBtn.setBounds(130, 80, 100, 40);
-        startBtn.addActionListener( e -> {
-
-            if (!inputPoly1.getText().isEmpty() && !inputPoly2.getText().isEmpty()) {
-                try {
-                    Main.runMainLoop(inputPoly1.getText(), inputPoly2.getText());
-                } catch (Exception ex) {
-                    Main.OUTPUT_TO_GUI = "No output to display (Error: " + ex.getMessage() + ")";
-                }
-            } else Main.OUTPUT_TO_GUI  = "No output to display (Empty Input)";
-
-            outputArea.setText(Main.OUTPUT_TO_GUI);
+        startBtn.addActionListener( new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!inputPoly1.getText().isEmpty() && !inputPoly2.getText().isEmpty()) {
+                    try {
+                        Main.runMainLoop(inputPoly1.getText(), inputPoly2.getText());
+                    } catch (Exception ex) {
+                        Main.OUTPUT_TO_GUI = "No output to display (Error: " + ex.getMessage() + ")";
+                        ex.printStackTrace();
+                    }
+                } else Main.OUTPUT_TO_GUI  = "No output to display (Empty Input)";
+    
+                outputArea.setText(Main.OUTPUT_TO_GUI);    
+            }
         });
 
         output_lbl = new JLabel("Output: ");
         output_lbl.setBounds(160, 140, 100, 20);
 
         outputArea = new JTextArea();
-        outputArea.setBounds(10, 160, 360, 290);
+        outputArea.setBounds(10, 160, 375, 290);
+        outputArea.setEditable(false);
+        
+        version_lbl = new JLabel("Lukas Schalk | 2017 | Version: " + Main.VERSION);
+        version_lbl.setBounds(10, 450, 300, 20);
 
 
         jf.add(inputPoly1_lbl);
@@ -68,6 +79,18 @@ public class GUI {
         jf.add(startBtn);
         jf.add(output_lbl);
         jf.add(outputArea);
+        jf.add(version_lbl);
+        
+        outputArea.setText("Wichtige Infos:\n-Alle Polynome müssen geordnet sein\n" +
+                           "-Es darf kein + oder - im Exponenten sein\n" + 
+                           "-Falls etwas vom Output abgeschnitten ist, steht auch nochmal alles\n" + 
+                           "  in der Konsole\n\n" + 
+                           "Test Polynome:\n" + 
+                           "5x^5-9x^4-3x^3+10x^2-2x : 2x^3-4x^2+2x\n" + 
+                           "x^3+6x^2+9x+4 : x+1\n" + 
+                           "x^3-5x^2-4x+8 : x-1\n" +
+                           "x^4+6x^3-4x^2-54x-45 : x-3\n"
+                           );
 
         jf.setVisible(true);
     }
