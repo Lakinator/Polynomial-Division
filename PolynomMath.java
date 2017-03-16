4234ff78db667d01a600d78a9653218c90354643
Lukas S <SchalkLukas20@gmail.com>
2017-03-16 22:35:50 +0100

    Update PolynomMath.java

--- PolynomMath.java
+++ PolynomMath.java
@@ -69,42 +69,47 @@ class PolynomMath {
     static String differenz(String minuend, String subtrahend) {
         String output = "";
 
+        for (double i = Polynom.deg(minuend); i >= 0; i--) {
+            if (!Helper.elementFromPolynomByExpo(minuend, i).equals("") && !Helper.elementFromPolynomByExpo(subtrahend, i).equals("")) {
+                output += _differenz(Helper.elementFromPolynomByExpo(minuend, i), Helper.elementFromPolynomByExpo(subtrahend, i));
+            } else if (!Helper.elementFromPolynomByExpo(minuend, i).equals("") && Helper.elementFromPolynomByExpo(subtrahend, i).equals("")) {
+                output += Helper.elementFromPolynomByExpo(minuend, i);
+            } else if (Helper.elementFromPolynomByExpo(minuend, i).equals("") && !Helper.elementFromPolynomByExpo(subtrahend, i).equals("")) {
 
-        //TODO: Neu machen
-
-        for (String einSubtrahend : Polynom.splitPolynom(subtrahend)) {
-
-            for (String einMinuend : Polynom.splitPolynom(minuend)) {
-                if (einMinuend.equals(einSubtrahend)) {
-                    output += "";
+                if (Polynom.getExponent(Helper.elementFromPolynomByExpo(subtrahend, i)) == 0) {
+                    output += _differenz("+0", Helper.elementFromPolynomByExpo(subtrahend, i));
+                } else if (Polynom.getExponent(Helper.elementFromPolynomByExpo(subtrahend, i)) != 0) {
+                    output += _differenz("+0x^" + Polynom.getExponent(Helper.elementFromPolynomByExpo(subtrahend, i)), Helper.elementFromPolynomByExpo(subtrahend, i));
                 }
+
             }
-        }
-
-        //Alt
-
-        for (String einMinuend : Polynom.splitPolynom(minuend)) {
-
-            for (String einSubtrahend : Polynom.splitPolynom(subtrahend)) {
-                if (Polynom.getExponent(einMinuend) != 0 && Polynom.getExponent(einSubtrahend) != 0 && Polynom.getExponent(einMinuend) == Polynom.getExponent(einSubtrahend) && !einMinuend.equals(einSubtrahend)) {
-                    double temp = Polynom.getFaktor(einMinuend) - Polynom.getFaktor(einSubtrahend);
-                    if (temp != 1.0 && temp != -1.0) output += temp >= 0 ? "+" + temp : temp;
-                    else output += temp == 1.0 ? "+" : "-";
-                    output += PolyMain.usedVariable; //Shiet
-                    if (Polynom.getExponent(einMinuend) != 1.0 && Polynom.getExponent(einMinuend) != -1.0) output += "^" + Polynom.getExponent(einMinuend);
-                    else if (Polynom.getExponent(einMinuend) == 1.0 || Polynom.getExponent(einMinuend) == -1.0) output += "";
-                } else if (Polynom.getExponent(einMinuend) == 0 && Polynom.getExponent(einSubtrahend) == 0 && !einMinuend.equals(einSubtrahend)) {
-                    double temp = Double.parseDouble(einMinuend) - Double.parseDouble(einSubtrahend);
-                    output += temp >= 0 ? "+" + temp : temp;
-                } else if (einMinuend.equals(einSubtrahend)) {
-                    output += "";
-                }
-            }
-
         }
 
         //System.out.println(minuend + " : " + subtrahend + " = " + output);
 
         return output;
+    }
+
+    static String _differenz(String einMinuend, String einSubtrahend) {
+        String _output = "";
+
+        //Selber Exponent!
+        if ( Polynom.getExponent(einMinuend) != Polynom.getExponent(einSubtrahend) ) return "";
+
+        if (Polynom.getExponent(einMinuend) != 0 && Polynom.getExponent(einSubtrahend) != 0 && !einMinuend.equals(einSubtrahend)) {
+            double temp = Polynom.getFaktor(einMinuend) - Polynom.getFaktor(einSubtrahend);
+            if (temp != 1.0 && temp != -1.0) _output += temp >= 0 ? "+" + temp : temp;
+            else _output += temp == 1.0 ? "+" : "-";
+            _output += PolyMain.usedVariable; //Shiet
+            if (Polynom.getExponent(einMinuend) != 1.0 && Polynom.getExponent(einMinuend) != -1.0) _output += "^" + Polynom.getExponent(einMinuend);
+            else if (Polynom.getExponent(einMinuend) == 1.0 || Polynom.getExponent(einMinuend) == -1.0) _output += "";
+        } else if (Polynom.getExponent(einMinuend) == 0 && Polynom.getExponent(einSubtrahend) == 0 && !einMinuend.equals(einSubtrahend)) {
+            double temp = Double.parseDouble(einMinuend) - Double.parseDouble(einSubtrahend);
+            _output += temp >= 0 ? "+" + temp : temp;
+        } else if (einMinuend.equals(einSubtrahend)) {
+            _output += "";
+        }
+
+        return _output;
     }
 }
