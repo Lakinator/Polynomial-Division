53e5dfd453db4dd9837a98f1b7aa5474258bc554
Lukas S <SchalkLukas20@gmail.com>
2017-03-17 19:43:29 +0100

    Update Polynom.java

--- Polynom.java
+++ Polynom.java
@@ -18,16 +18,34 @@ class Polynom {
         this.wert = wert;
     }
 
-    String clean() {
+    String getWert() {
+        return wert;
+    }
+
+    void setWert(String wert) {
+        this.wert = wert;
+    }
+
+    /**
+     * @return
+     *        A cleaned version of the saved String in the Polynom
+     */
+    String cleaned() {
         return Helper.clean(this.wert);
     }
 
+    /**
+     * Just to complicated to explain. It basically splits a Polynom String into its pieces
+     * @param in
+     *        The String that should be splitted
+     * @return
+     *        An Array with the splitted parts
+     */
     static String[] splitPolynom(String in) {
         String[] out_java7;    //Only needed if running on java 7
 
 
         String[] out = Helper.trimAll(in).split("(?=[+-])");
-        //System.out.println("Länge " + out.length);
         if (out.length == 0) return new String[0];
 
         if (out[0].isEmpty() && out.length > 1) {
@@ -38,17 +56,24 @@ class Polynom {
                 out_java7[i] = out[i + 1];
             }
 
-            if (!out_java7[0].startsWith("+") && !out_java7[0].startsWith("-") ) out[0] = "+" + out[0];
+            if (!out_java7[0].startsWith("+") && !out_java7[0].startsWith("-") ) out[0] = "+" + out[0]; // Adds a "+" if there is no "+" or "-" in front of the first element
             return out_java7;
         } else {
             //System.out.println("Java 8");
 
-            if (!out[0].startsWith("+") && !out[0].startsWith("-") ) out[0] = "+" + out[0];
+            if (!out[0].startsWith("+") && !out[0].startsWith("-") ) out[0] = "+" + out[0]; // Adds a "+" if there is no "+" or "-" in front of the first element
         }
 
         return out;
     }
 
+    /**
+     *
+     * @param s
+     *        The Polynom String element from which you want to know the degree
+     * @return
+     *        The Polynom degree
+     */
     static Double deg(String s) {
 
         double highest = 0;
@@ -65,6 +90,13 @@ class Polynom {
         return highest;
     }
 
+    /**
+     *
+     * @param in
+     *        The Polynom String element from which you want to know the factor
+     * @return
+     *        The factor from the element
+     */
     static double getFaktor(String in) {
         if(in.matches("[+-][0-9]+[.]*[0-9]*[a-z]\\^[0-9]+[.]*[0-9]*")) {
             return Double.parseDouble(in.split("[a-z]")[0]);
@@ -81,6 +113,13 @@ class Polynom {
         }
     }
 
+    /**
+     *
+     * @param in
+     *        The Polynom String element from which you want to know the exponent
+     * @return
+     *        The exponent from the element
+     */
     static double getExponent(String in) {
         if(in.matches("[+-][0-9]+[.]*[0-9]*[a-z]\\^[0-9]+[.]*[0-9]*")) {
             return Double.parseDouble(in.split("\\^")[1]);
@@ -99,11 +138,4 @@ class Polynom {
         }
     }
 
-    String getWert() {
-        return wert;
-    }
-
-    void setWert(String wert) {
-        this.wert = wert;
-    }
 }
