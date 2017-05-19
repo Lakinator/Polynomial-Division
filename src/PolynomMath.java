b166f25a844779188f4ae70fddf56ff0028e70c9
Lukas S <SchalkLukas20@gmail.com>
2017-05-19 21:26:12 +0200

    Version 1.0

--- src/PolynomMath.java
+++ src/PolynomMath.java
@@ -6,23 +6,77 @@ package Polynomdivision;
  */
 
 class PolynomMath {
+
+    /**
+     * This calculates the division between two values
+     * There are 4 cases:
+     * [1]:
+     *     Dividend: - Only a normal value (1, 2, 3)
+     *               - No exponent
+     *               --> Number
+     *     Divisor:  - Only a normal value (1, 2, 3)
+     *               - No exponent
+     *               --> Number
+     *
+     *     Calculation:
+     *                 - Number / number
+     *
+     * [2]:
+     *     Dividend: - The value contains a variable (1x, 2x,3x)
+     *               - The value has got an exponent (x^1, x^2, x^3)
+     *               --> Monom
+     *     Divisor:  - The value contains a variable (1x, 2x,3x)
+     *               - The value has got an exponent (x^1, x^2, x^3)
+     *               --> Monom
+     *
+     *     Calculation:
+     *                 - Coefficient / coefficient
+     *                         +
+     *                 - usedVariable
+     *                         +
+     *                 - Exponent - exponent
+     *
+     * [3]:
+     *     Dividend: - The value contains a variable (1x, 2x,3x)
+     *               - The value has got an exponent (x^1, x^2, x^3)
+     *               --> Monom
+     *     Divisor:  - Only a normal value (1, 2, 3)
+     *               - No exponent
+     *               --> Number
+     *
+     *     Calculation:
+     *                 - Coefficient / number
+     *                         +
+     *                 - usedVariable
+     *                         +
+     *                 - exponent
+     *
+     *
+     *
+     * @param dividend
+     *        The dividend (only a polynom element)
+     * @param divisor
+     *        The divisor (only a polynom element)
+     * @return
+     *        The result
+     */
     static String division(String dividend, String divisor) {
         String output = "";
 
+        /*[1]*/
         if (Polynom.getCoefficient(dividend) == 0 && Polynom.getCoefficient(divisor) == 0 && Polynom.getExponent(dividend) == 0 && Polynom.getExponent(divisor) == 0) {
             double temp = Double.parseDouble(dividend) / Double.parseDouble(divisor);
             output += temp >= 0 ? "+" + temp : temp;
-        } else if (Polynom.getCoefficient(dividend) != 0 && Polynom.getCoefficient(divisor) != 0 && Polynom.getExponent(dividend) != 0 && Polynom.getExponent(divisor) != 0) {
+        }
+        /*[2]*/
+        else if (Polynom.getCoefficient(dividend) != 0 && Polynom.getCoefficient(divisor) != 0 && Polynom.getExponent(dividend) != 0 && Polynom.getExponent(divisor) != 0) {
             double temp = Polynom.getCoefficient(dividend) / Polynom.getCoefficient(divisor);
             output += temp >= 0 ? "+" + temp : temp;
             output += PolynomMain.usedVariable + "^";
             output += Polynom.getExponent(dividend) - Polynom.getExponent(divisor);
-        } else if (Polynom.getCoefficient(dividend) == 0 && Polynom.getCoefficient(divisor) != 0 && Polynom.getExponent(dividend) == 0 && Polynom.getExponent(divisor) != 0) {
-            double temp = Double.parseDouble(dividend) / Polynom.getCoefficient(divisor);
-            output += temp >= 0 ? "+" + temp : temp;
-            output += PolynomMain.usedVariable + "^";
-            output += Polynom.getExponent(divisor);
-        } else if (Polynom.getCoefficient(dividend) != 0 && Polynom.getCoefficient(divisor) == 0 && Polynom.getExponent(dividend) != 0 && Polynom.getExponent(divisor) == 0) {
+        }
+        /*[3]*/
+        else if (Polynom.getCoefficient(dividend) != 0 && Polynom.getCoefficient(divisor) == 0 && Polynom.getExponent(dividend) != 0 && Polynom.getExponent(divisor) == 0) {
             double temp = Polynom.getCoefficient(dividend) / Double.parseDouble(divisor);
             output += temp >= 0 ? "+" + temp : temp;
             output += PolynomMain.usedVariable + "^";
@@ -35,51 +89,170 @@ class PolynomMath {
 
     }
 
-    static String multiplizieren(String multiplikator, String multiplikand) {
+
+
+    /**
+     * This calculates the multiplication between two values
+     * The method iterates over all elements of the multiplicand
+     * There are 4 cases:
+     * [1]:
+     *     Multiplier:    - Only a normal value (1, 2, 3)
+     *                    - No exponent
+     *                    --> Number
+     *     Multiplicand:  - Only a normal value (1, 2, 3)
+     *                    - No exponent
+     *                    --> Number
+     *
+     *     Calculation:
+     *                 - Number * number
+     *
+     * [2]:
+     *     Multiplier:    - The value contains a variable (1x, 2x,3x)
+     *                    - The value has got an exponent (x^1, x^2, x^3)
+     *                    --> Monom
+     *     Multiplicand:  - The value contains a variable (1x, 2x,3x)
+     *                    - The value has got an exponent (x^1, x^2, x^3)
+     *                    --> Monom
+     *
+     *     Calculation:
+     *                 - Coefficient * coefficient
+     *                         +
+     *                 - usedVariable
+     *                         +
+     *                 - Exponent + exponent
+     *
+     * [3]:
+     *     Multiplier:    - Only a normal value (1, 2, 3)
+     *                    - No exponent
+     *                    --> Number
+     *     Multiplicand:  - The value contains a variable (1x, 2x,3x)
+     *                    - The value has got an exponent (x^1, x^2, x^3)
+     *                    --> Monom
+     *
+     *     Calculation:
+     *                 - Number * coefficient
+     *                         +
+     *                 - usedVariable
+     *                         +
+     *                 - exponent
+     *
+     * [4]:
+     *     Multiplier:    - The value contains a variable (1x, 2x,3x)
+     *                    - The value has got an exponent (x^1, x^2, x^3)
+     *                    --> Monom
+     *     Multiplicand:  - Only a normal value (1, 2, 3)
+     *                    - No exponent
+     *                    --> Number
+     *
+     *     Calculation:
+     *                 - Coefficient * number
+     *                         +
+     *                 - usedVariable
+     *                         +
+     *                 - exponent
+     *
+     *
+     *
+     * @param multiplier
+     *        The multiplier (only a polynom element)
+     * @param multiplicand
+     *        The multiplicand (can be a full polynom)
+     * @return
+     *        The result
+     */
+    static String multiply(String multiplier, String multiplicand) {
         String output = "";
 
-        for (String s : Polynom.splitPolynom(multiplikand)) {
+        for (String s : Polynom.splitPolynom(multiplicand)) {
 
-            if (Polynom.getCoefficient(multiplikator) == 0 && Polynom.getCoefficient(s) == 0 && Polynom.getExponent(multiplikator) == 0 && Polynom.getExponent(s) == 0) {
-                double temp = Double.parseDouble(s) * Double.parseDouble(multiplikator);
+            /*[1]*/
+            if (Polynom.getCoefficient(multiplier) == 0 && Polynom.getCoefficient(s) == 0 && Polynom.getExponent(multiplier) == 0 && Polynom.getExponent(s) == 0) {
+                double temp = Double.parseDouble(s) * Double.parseDouble(multiplier);
                 output += temp >= 0 ? "+" + temp : temp;
-            } else if (Polynom.getCoefficient(multiplikator) != 0 && Polynom.getCoefficient(s) != 0 && Polynom.getExponent(multiplikator) != 0 && Polynom.getExponent(s) != 0) {
-                double temp = Polynom.getCoefficient(multiplikator) * Polynom.getCoefficient(s);
+            }
+            /*[2]*/
+            else if (Polynom.getCoefficient(multiplier) != 0 && Polynom.getCoefficient(s) != 0 && Polynom.getExponent(multiplier) != 0 && Polynom.getExponent(s) != 0) {
+                double temp = Polynom.getCoefficient(multiplier) * Polynom.getCoefficient(s);
                 output += temp >= 0 ? "+" + temp : temp;
                 output += PolynomMain.usedVariable + "^";
-                output += Polynom.getExponent(multiplikator) + Polynom.getExponent(s);
-            } else if (Polynom.getCoefficient(multiplikator) == 0 && Polynom.getCoefficient(s) != 0 && Polynom.getExponent(multiplikator) == 0 && Polynom.getExponent(s) != 0) {
-                double temp = Double.parseDouble(multiplikator) * Polynom.getCoefficient(s);
+                output += Polynom.getExponent(multiplier) + Polynom.getExponent(s);
+            }
+            /*[3]*/
+            else if (Polynom.getCoefficient(multiplier) == 0 && Polynom.getCoefficient(s) != 0 && Polynom.getExponent(multiplier) == 0 && Polynom.getExponent(s) != 0) {
+                double temp = Double.parseDouble(multiplier) * Polynom.getCoefficient(s);
                 output += temp >= 0 ? "+" + temp : temp;
                 output += PolynomMain.usedVariable + "^";
                 output += Polynom.getExponent(s);
-            } else if (Polynom.getCoefficient(multiplikator) != 0 && Polynom.getCoefficient(s) == 0 && Polynom.getExponent(multiplikator) != 0 && Polynom.getExponent(s) == 0) {
-                double temp = Polynom.getCoefficient(multiplikator) * Double.parseDouble(s);
+            }
+            /*[4]*/
+            else if (Polynom.getCoefficient(multiplier) != 0 && Polynom.getCoefficient(s) == 0 && Polynom.getExponent(multiplier) != 0 && Polynom.getExponent(s) == 0) {
+                double temp = Polynom.getCoefficient(multiplier) * Double.parseDouble(s);
                 output += temp >= 0 ? "+" + temp : temp;
                 output += PolynomMain.usedVariable + "^";
-                output += Polynom.getExponent(multiplikator);
+                output += Polynom.getExponent(multiplier);
             }
         }
 
-        //System.out.println(multiplikator + " * " + multiplikand + " = " + output);
+        //System.out.println(multiplier + " * " + multiplicand + " = " + output);
 
         return output;
     }
 
-    static String differenz(String minuend, String subtrahend) {
+
+
+    /**
+     * This calculates the difference between two polynoms
+     * The method iterates over all elements of the two polynoms
+     * There are 3 cases:
+     * [1]:
+     *     Minuend:     - A monom with the same degree as the iterating value
+     *     Subtrahend:  - A monom with the same degree as the iterating value
+     *
+     *     Calculation:
+     *                 - _difference()
+     *
+     * [2]:
+     *     Minuend:     - A monom with the same degree as the iterating value
+     *     Subtrahend:  - No monom with the same degree as the iterating value
+     *
+     *     Calculation:
+     *                 - Add the minuend
+     *
+     * [2]:
+     *     Minuend:     - No monom with the same degree as the iterating value
+     *     Subtrahend:  - A monom with the same degree as the iterating value
+     *
+     *     Calculation:
+     *                 - _difference() -> subtracting the subtrahend from 0
+     *
+     *
+     *
+     * @param minuend
+     *        The minuend (can be a full polynom)
+     * @param subtrahend
+     *        The subtrahend (can be a full polynom)
+     * @return
+     *        The result
+     */
+    static String difference(String minuend, String subtrahend) {
         String output = "";
 
         for (double i = Polynom.deg(minuend); i >= 0; i--) {
+            /*[1]*/
             if (!Helper.elementFromPolynomByExpo(minuend, i).equals("") && !Helper.elementFromPolynomByExpo(subtrahend, i).equals("")) {
-                output += _differenz(Helper.elementFromPolynomByExpo(minuend, i), Helper.elementFromPolynomByExpo(subtrahend, i));
-            } else if (!Helper.elementFromPolynomByExpo(minuend, i).equals("") && Helper.elementFromPolynomByExpo(subtrahend, i).equals("")) {
+                output += _difference(Helper.elementFromPolynomByExpo(minuend, i), Helper.elementFromPolynomByExpo(subtrahend, i));
+            }
+            /*[2]*/
+            else if (!Helper.elementFromPolynomByExpo(minuend, i).equals("") && Helper.elementFromPolynomByExpo(subtrahend, i).equals("")) {
                 output += Helper.elementFromPolynomByExpo(minuend, i);
-            } else if (Helper.elementFromPolynomByExpo(minuend, i).equals("") && !Helper.elementFromPolynomByExpo(subtrahend, i).equals("")) {
+            }
+            /*[3]*/
+            else if (Helper.elementFromPolynomByExpo(minuend, i).equals("") && !Helper.elementFromPolynomByExpo(subtrahend, i).equals("")) {
 
                 if (Polynom.getExponent(Helper.elementFromPolynomByExpo(subtrahend, i)) == 0) {
-                    output += _differenz("+0", Helper.elementFromPolynomByExpo(subtrahend, i));
+                    output += _difference("+0", Helper.elementFromPolynomByExpo(subtrahend, i));
                 } else if (Polynom.getExponent(Helper.elementFromPolynomByExpo(subtrahend, i)) != 0) {
-                    output += _differenz("+0x^" + Polynom.getExponent(Helper.elementFromPolynomByExpo(subtrahend, i)), Helper.elementFromPolynomByExpo(subtrahend, i));
+                    output += _difference("+0x^" + Polynom.getExponent(Helper.elementFromPolynomByExpo(subtrahend, i)), Helper.elementFromPolynomByExpo(subtrahend, i));
                 }
 
             }
@@ -90,7 +263,7 @@ class PolynomMath {
         return output;
     }
 
-    static String _differenz(String einMinuend, String einSubtrahend) {
+    static String _difference(String einMinuend, String einSubtrahend) {
         String _output = "";
 
         //Selber Exponent!
